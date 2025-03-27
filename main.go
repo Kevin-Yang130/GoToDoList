39986dda27342d71876a57210fbf1f2bf8685931
Kevin Yang-Li <yanglikevin@gmail.com>
2025-03-27 12:48:51 -0400

    wrote a simple list of items

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Testing connection between local env and github repo")
+	var taskOne = "Finish CIS2620 Homework"
+	var taskTwo = "Finish Goland Tutorial"
+	var taskThree = "Eat food"
+	var newLine = ""
+
+	fmt.Println("####### To Do List ######")
+	fmt.Println(newLine)
+
+	fmt.Println(taskOne)
+	fmt.Println(taskTwo)
+	fmt.Println(taskThree)
 }
